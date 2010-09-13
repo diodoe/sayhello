4a38467e0f46b6ff01aab45d9d1a451996e15c31
ifabio <me@ifabio.com>
2010-09-13 17:41:05 +0200

    Some styles for forms

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,11 @@ Spork.prefork do
     ### Part of a Spork hack. See http://bit.ly/arY19y
     # Emulate initializer set_clear_dependencies_hook in 
     # railties/lib/rails/application/bootstrap.rb
-    ActiveSupport::Dependencies.clear
+    ActiveSupport::Dependencies.clear            
+    
+    def test_sign_in(user)
+        controller.current_user = user
+    end
   end
 end
 
