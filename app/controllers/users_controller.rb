8da897bb976720827ee7e963c3e6e6133393f773
ifabio <me@ifabio.com>
2010-09-13 12:39:38 +0200

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+  
   def new                 
+    @user=User.new
     @title = "Sign up"
   end                  
+  
   def show 
     @user = User.find(params[:id])   
     @title = @user.name
-  end
-
+  end  
+  
+  def create
+      @user = User.new(params[:user])
+      if @user.save                                  
+        flash[:success] = "Welcome to the Sample App!"
+        #user_path instead @user, because of rspec
+        redirect_to user_path(@user)
+      else
+        @title = "Sign up"
+        render 'new'
+      end
+    end
 end
